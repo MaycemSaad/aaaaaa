6797031815fb63d91751c7bffaea26088ef624a5
faresbrayek2 <faresbrayek2@gmail.com>
2024-03-02 23:58:13 +0100

    helllo

--- src/Controller/ProjetsController.php
+++ src/Controller/ProjetsController.php
@@ -10,58 +10,66 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CategoryRepository; // Assuming you have a Category entity and corresponding repository
 
 #[Route('/projets')]
 class ProjetsController extends AbstractController
 {
     #[Route('/category/{id}', name: 'app_projets_index', methods: ['GET'])]
-    public function index(ProjetsRepository $projetsRepository , Request $request ): Response
+    public function index(ProjetsRepository $projetsRepository , Request $request , int $id): Response
     {
         $categoryId = $request->attributes->get('id'); 
         $projets = $projetsRepository->findBy(['category' => $categoryId]);
         return $this->render('projets/index.html.twig', [
             'projets' => $projets,
+            'categoryId' => $categoryId,
         ]);
     }
 
-    #[Route('/new', name: 'app_projets_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $projet = new Projets();
-        if(!$projet->getDateDeCreation()){
-            $projet->setDateDeCreation(new \DateTime());}
-    
-            $form = $this->createForm(ProjetsType::class, $projet);
-            $form->handleRequest($request);
-    
-            if ($form->isSubmitted() && $form->isValid()) {
-                $photoFile = $form->get('photoURL')->getData();
-    
-                // Vérifiez si un fichier a été téléchargé
-                if ($photoFile) {
-                    // Générez un nom de fichier unique
-                    $newFilename = uniqid().'.'.$photoFile->guessExtension();
-    
-                    // Déplacez le fichier vers le répertoire où sont stockées les photos
-                    $photoFile->move(
-                        $this->getParameter('photos_directory'),
-                        $newFilename
-                    );
-    
-                    // Mettez à jour l'URL de l'image dans l'entité Project
-                    $projet->setPhotoUrl($newFilename);
-                }
-                $entityManager->persist($projet);
-                $entityManager->flush();
-    
-                return $this->redirectToRoute('app_projets_index', [], Response::HTTP_SEE_OTHER);
-            }
-    
-        return $this->render('projets/new.html.twig', [
-            'projet' => $projet,
-            'form' => $form->createView(),
-        ]);
+#[Route('/category/{categoryId}/new', name: 'app_projets_new', methods: ['GET', 'POST'])]
+public function new(Request $request, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository, int $categoryId): Response
+{
+    $category = $categoryRepository->find($categoryId);
+
+    if (!$category) {
+        throw $this->createNotFoundException('The category does not exist');
     }
+
+    $projet = new Projets();
+    $projet->setCategory($category); // Assuming you have a setCategory method in your Projets entity
+
+    if(!$projet->getDateDeCreation()){
+        $projet->setDateDeCreation(new \DateTime());
+    }
+
+    $form = $this->createForm(ProjetsType::class, $projet);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $photoFile = $form->get('photoURL')->getData();
+
+        if ($photoFile) {
+            $newFilename = uniqid().'.'.$photoFile->guessExtension();
+            $photoFile->move(
+                $this->getParameter('photos_directory'),
+                $newFilename
+            );
+            $projet->setPhotoUrl($newFilename);
+        }
+
+        $entityManager->persist($projet);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_projets_index', ['id' => $categoryId], Response::HTTP_SEE_OTHER);
+    }
+
+    return $this->render('projets/new.html.twig', [
+        'projet' => $projet,
+        'form' => $form->createView(),
+            'categoryId' => $categoryId, // Make sure this line is included
+
+    ]);
+}
 
     #[Route('/{id}', name: 'app_projets_show', methods: ['GET'])]
     public function show(Projets $projet): Response
@@ -72,7 +80,7 @@ class ProjetsController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_projets_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Projets $projet, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Projets $projet, EntityManagerInterface $entityManager,int $id): Response
     {
         $form = $this->createForm(ProjetsType::class, $projet);
         $form->handleRequest($request);
@@ -93,13 +101,16 @@ class ProjetsController extends AbstractController
                 $projet->setPhotoUrl($newFilename);
             }
             $entityManager->flush();
+            // get the category id from the project
+            $categoryId = $projet->getCategory()->getId();  
 
-            return $this->redirectToRoute('app_projets_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_projets_index', ['id' => $categoryId], Response::HTTP_SEE_OTHER);
         }
         
         return $this->render('projets/edit.html.twig', [
             'projet' => $projet,
             'form' => $form->createView(),
+            'categoryId' => $id, //
         ]);
     }
 
